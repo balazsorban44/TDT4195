368105d212b133065c80fb9d146c6944eaff55e4
Balázs Orbán <info@balazsorban.com>
2018-09-04 09:27:49 +0200

    🎨 new coordinates/better structure
    added comments for better understanding

--- Assignment_1/gloom/gloom/src/program.cpp
+++ Assignment_1/gloom/gloom/src/program.cpp
@@ -7,26 +7,27 @@
 
 //  Coordinates DATA
 float coords_1[] = {
-    0.25, 0.25, 0,
-    1, 0, 0,
-    0, 1, 0,
+    // Upper right
+    0.25, 0.25, 0, // 0
+    1, 0, 0, // 1
+    0, 1, 0, // 2
 
-    -0.25, -0.25, 0,
-    -1, 0, 0,
-    0, -1, 0,
+    // Lower right
+    -0.25, -0.25, 0, // 3
+    -1, 0, 0, // 4
+    0, -1, 0, // 5
 
-    0.25, -0.25, 0,
-    0, -1, 0,
-    1, 0, 0,
+    // Lower right
+    0.25, -0.25, 0, // 6
 
-    -0.25, 0.25, 0,
-    0, 1, 0,
-    -1, 0, 0,
+    // Upper left 
+    -0.25, 0.25, 0, // 7
+    0, 1, 0, // 8
 
-    
-    -0.2, -0.2, 0,
-    0, -0.2, 0,
-    -0.2, 0, 0,
+    // Middle
+    -0.2, -0.2, 0, // 9
+    0, -0.2, 0, // 10
+    -0.2, 0, 0, // 11
 };
 
 float coords_2[] = {
@@ -35,10 +36,25 @@ float coords_2[] = {
     -0.8, -0.2, 1.2
 };
 
-unsigned int indexes_1[] = {0,1,2, 3,4,5, 6,7,8, 9,10,11, 12,13,14};
-unsigned int indexes_2[] = {0,1,2};
+float coords_3[] = {
+    -0.6, -0.6, 0,
+     0.6, -0.6, 0,
+     0,    0.6, 0
+};
 
+unsigned int indices_1[] = {
+    0,1,2, // Upper right
+    3,4,5, // Lower left
+    1,6,5, // Lower right
+    4,7,8, // Upper left
+    9,10,11 // Middle
+};
+unsigned int indices_2[] = {0,1,2};
 
+int coords_1_count = sizeof(coords_1)/sizeof(*coords_1);
+int coords_2_count = sizeof(coords_2)/sizeof(*coords_2);
+int indices_1_count = sizeof(indices_1)/sizeof(*indices_1);
+int indices_2_count = sizeof(indices_2)/sizeof(*indices_2);
 
 
 
