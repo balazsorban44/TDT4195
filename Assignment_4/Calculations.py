771141af27a240655ebf3ee4a857c31c119c7f3d
MargotSirdey <margot.sirdey@insa-rouen.fr>
2018-10-29 17:02:26 +0100

    calculationsModif

--- Assignment_4/Calculations.py
+++ Assignment_4/Calculations.py
@@ -15,11 +15,11 @@ def initialize_weights(n_x, n_h, n_y):
   W2 = np.random.normal(0, 1/np.sqrt(n_h), (n_y, n_h))
   assert (W1.shape == (n_h, n_x))
   assert (W2.shape == (n_y, n_h))
-  
+
   parameters = {"w1": W1,
                 "w2": W2}
   return parameters
-  
+
 
 def initialize_parameters(n_x, n_h, n_y):
   b1 = np.zeros((n_h, 1))
@@ -35,18 +35,17 @@ def weighted_sum(weights, data):
 
 
 def forward_propagation(input_1st_layer, parameters):
-  
+
   sum_1st_neuron_1st_layer = np.add(weighted_sum(parameters["w1"], input_1st_layer)[0], parameters["b1"][0])
   sum_2nd_neuron_1st_layer = np.add(weighted_sum(parameters["w1"], input_1st_layer)[1], parameters["b1"][1])
-  
-  
+
   ouput_1st_neuron_1st_layer = sigmoid(sum_1st_neuron_1st_layer)
   ouput_2nd_neuron_1st_layer = sigmoid(sum_2nd_neuron_1st_layer)
 
   input_2nd_layer = np.array([ouput_1st_neuron_1st_layer, ouput_2nd_neuron_1st_layer])
   sum_1st_neuron_2nd_layer = np.add(weighted_sum(parameters["w2"], input_2nd_layer)[0], parameters["b2"][0])
   sum_2nd_neuron_2nd_layer = np.add(weighted_sum(parameters["w2"], input_2nd_layer)[1], parameters["b2"][1])
-  
+
   ouput_1st_neuron_2nd_layer = sigmoid(sum_1st_neuron_2nd_layer)
   ouput_2nd_neuron_2nd_layer = sigmoid(sum_2nd_neuron_2nd_layer)
 
@@ -72,21 +71,26 @@ def backward_propagation(parameters, cache, X, Y):
   S11 = cache["s11"]
   S21 = cache["s21"]
 
-  W12 = parameters["w2"][0]
-  W22 = parameters["w2"][1]
-
+  W12 = np.transpose(parameters["w2"])[0]
+  W22 =  np.transpose(parameters["w2"])[1]
+  print(X)
   d12 = (O12 - Y) * sigmoid(S12) * (1 - sigmoid(S12))
   d22 = (O22 - Y) * sigmoid(S22) * (1 - sigmoid(S22))
 
-  print(W12, d12, S11)
-  d11 = W12.T * d12 * sigmoid(S11) * (1 - sigmoid(S11))
-  d21 = W22.T * d22 * sigmoid(S21) * (1 - sigmoid(S21))
+
+  #d11 = W12.T * d12 * sigmoid(S11) * (1 - sigmoid(S11))
+
+  d11 = W12 * d12 * np.multiply(sigmoid(S11),(1 - sigmoid(S11)))
+
+
+  d21 = W22 * d22 * np.multiply(sigmoid(S21),(1 - sigmoid(S21)))
+
 
   partial_derivative12 = d12 * S21
   partial_derivative22 = d22 * S11
-  partial_derivative11 = d11 * X.T
-  partial_derivative21 = d21 * X.T
-  
+  partial_derivative11 = d11 * X
+  partial_derivative21 = d21 * np.transpose(X)
+
   return {
     "pd12": partial_derivative12,
     "pd22": partial_derivative22,
@@ -94,62 +98,51 @@ def backward_propagation(parameters, cache, X, Y):
     "pd21": partial_derivative21
   }
 
-# def backward_propagation(parameters, cache, X, Y):
-  
-#     m = 1 / X.shape[0]
-    
-#     W2 = parameters["w2"]
-        
-#     S11 = cache["o11"]
-#     S21 = cache["o21"]
-#     S12 = cache["o12"]
-#     S22 = cache["o22"]
-    
-#     # Second layer
-#     dZ12 = S12 - Y
-#     dZ22 = S22 - Y
+def update_parameters(parameters, grads, learning_rate = 0.5):
 
-#     print(dZ12, S11)
-#     dW12 = m * np.dot(dZ12, S21.T)
-#     dW22 = m * np.dot(dZ22, S11.T)
+    W11 = parameters["w1"][0]
+    W21 = parameters["w1"][1]
+    W12 = parameters["w2"][0]
+    W22 = parameters["w2"][1]
+    b1 = parameters["b1"]
+    b2 = parameters["b2"]
 
-#     db12 = m * np.sum(dZ12, axis=1, keepdims=True)
-#     db22 = m * np.sum(dZ22, axis=1, keepdims=True)
+    dW11 = grads["pd11"]
+    dW21 = grads["pd21"]
+    dW12 = grads["pd12"]
+    dW22 = grads["pd22"]
 
-#     # First layer
-#     dZ11 = np.multiply(np.dot(W2.T, dZ12), 1 - np.power(S11, 2))
-#     dZ21 = np.multiply(np.dot(W2.T, dZ22), 1 - np.power(S21, 2))
+    dB1 = 0
+    dB2 = 0
+    W11 = W11 - learning_rate * dW11
+    W21 = W21 - learning_rate * dW21
+    W12 = W12 - learning_rate * dW12
+    W22 = W22 - learning_rate * dW22
+    b1 = b1 - learning_rate * dB1
+    b2 = b2 - learning_rate * dB2
 
-#     dW11 = m * np.dot(dZ11, X.T)
-#     dW21 = m * np.dot(dZ21, X.T)
+    new_parameters = {"w1": np.array([W11,W21]),
+                    "w2": np.array([W12,W22]),
+                    "b1": b1,
+                    "b2": b2}
 
-#     db11 = m * np.sum(dZ11, axis=1, keepdims=True)
-#     db21 = m * np.sum(dZ21, axis=1, keepdims=True)
-    
-#     return {
-#       "dW11": dW11,
-#       "dW21": dW21,
-#       "db11": db11,
-#       "db21": db21,
-#       "dW12": dW12,
-#       "dW22": dW22,
-#       "db12": db12,
-#       "db22": db22
-#     }
+    return new_parameters
 
 if __name__ == '__main__':
-  data = np.array([156, 140, 180, 192, 172, 162])
+  data = np.array([0.1, 0.90, 0.2, 0.4, 0.6])
   # expected_output = np.array([1, 0])
-  expected_output = np.array([1, 0, 1, 1, 0, 1])
+  expected_output = np.array([0, 1,0,1,1])
   output = None
 
   cache = None
 
-  parameters = initialize_parameters(np.size(data), 2, 2)
+  parameters = initialize_parameters(np.size(data), 2, 5)
 
 
-  for i in range(10):
+
+  for i in range(5):
     output, cache = forward_propagation(data, parameters)
     grads = backward_propagation(parameters, cache, output, expected_output)
-
-  print(grads)
+    parameters = update_parameters(parameters,grads)
+    print(parameters)
+print(output)
